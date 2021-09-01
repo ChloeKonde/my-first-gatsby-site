d060a6d1927703bb8c515acc1600fb98ff1064a0
konde.chloe <konde.chloe@gmail.com>
2021-09-01 19:28:18 +0600

    finish the second part of gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "test",
+    title: "My First Gatsby Site",
   },
   plugins: ["gatsby-plugin-gatsby-cloud"],
 };
